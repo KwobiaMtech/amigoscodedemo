bbbdde3dc6fc6d219f430470a58e39c731bada57
Patrick Oduro <poduro@mtechcomm.com>
2020-01-20 10:19:00 +0000

    Initial Commit

---
+++ src/main/java/com/amigoscode/amigoscode/student/StudentController.java
@@ -0,0 +1,24 @@
+package com.amigoscode.amigoscode.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return  List.of(
+                new Student(UUID.randomUUID(), "James","Bond","jamesbond@gmail.com", Student.Gender.MALE),
+                new Student(UUID.randomUUID(), "Patrick","Oduro","patrickoduro@gmail.com", Student.Gender.MALE),
+                new Student(UUID.randomUUID(), "Joyce","Addo","joyce@gmail.com", Student.Gender.FEMALE)
+
+        );
+    }
+
+}
---
+++ src/test/java/com/amigoscode/amigoscode/AmigoscodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.amigoscode.amigoscode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmigoscodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
